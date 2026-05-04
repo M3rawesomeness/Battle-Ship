00e3681c1ef68d9d5f55e2a541bfb54bb99f724a
PC <davoudi.kian2007@gmail.com>
2026-05-04 02:15:14 -0400

    Refactored and fixed some bugs

--- BattleShip.cpp
+++ BattleShip.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define HIT 0
 #define OCC 1 // Occupied
 #define MIS 1
+#define SCAN 2
 #define GRID_DIM 12
 #define UP 'u'
 #define DOWN 'd'
@@ -15,6 +16,7 @@ using namespace std;
 #define CLEAR_SCREEN "cls"
 #define HIT_SYMBOL "x"
 #define SHIP_SYMBOL "o"
+#define SCAN_SYMBOL "*"
 #define LINE_ATTACK 1
 #define SCAN_ATTACK 2
 #define MINI_ATTACK 3
@@ -53,17 +55,22 @@ void ask_coords(int *x, int *y)
  * Ask only for coordinate with no direction
  */
 {
+    constexpr int offset = 1;
     cout << "Enter X Coord: ";
     cin >> *x;
     cout << "Enter Y Coord: ";
     cin >> *y;
+    *x -= offset;
+    *y -= offset;
 }
 
-void ask_coord_dir(int *x,  int *y, char *dir)
+void ask_coords_dir(int *x,  int *y, char *dir)
 /*
  * Ask for the coordinates with direction
  */
 {
+    constexpr int offset = 1;
+
     cout << "Invalid Coord, please try again" << endl;
     cout << "Enters X coord: ";
     cin >> *y;
@@ -71,6 +78,8 @@ void ask_coord_dir(int *x,  int *y, char *dir)
     cin >> *x;
     cout << "Enter Dir: ";
     cin >> *dir;
+    *x -= offset;
+    *y -= offset;
 }
 
 void print_line(const int len)
@@ -104,17 +113,13 @@ void print_screen(int grid[GRID_DIM][GRID_DIM], int num_ships)
         for (int j = 0; j < GRID_DIM; j++)
         {
             if (grid[i][j] == OCC)
-            {
-                cout << "o";
-            }
+                cout << SHIP_SYMBOL;
             else if (grid[i][j] == HIT)
-            {
-                cout << "x";
-            }
+                cout << HIT_SYMBOL;
+            else if (grid[i][j] == SCAN)
+                cout << SCAN_SYMBOL;
             else
-            {
                 cout << " ";
-            }
             cout << " | ";
         }
         cout << endl;
@@ -129,6 +134,7 @@ bool valid_dir(const char dir, const int len, const int x, const int y)
 
     if (dir != RIGHT && dir != LEFT && dir != DOWN && dir != UP)
         return false;
+
     if (dir == RIGHT && x + len > upper_bound)
         return false;
     if (dir == LEFT && x - len < lower_bound)
@@ -140,44 +146,33 @@ bool valid_dir(const char dir, const int len, const int x, const int y)
     return true;
 }
 
-bool check_coord_bounds(int x, int y)
-{
-    constexpr int upper_bound = 12;
-    constexpr int lower_bound = 0;
-
-    return (x > upper_bound || x < lower_bound ||  y > upper_bound || y < lower_bound);
-}
-
-void validate_update_coords(int grid[GRID_DIM][GRID_DIM], int *x, int *y)
+bool check_coord_bounds(const int x, const int y)
 {
     constexpr int upper_bound = 11;
     constexpr int lower_bound = 0;
 
+    return (x >= lower_bound && x <= upper_bound &&  y >= lower_bound && y <= upper_bound);
+}
+
+void validate_update_coords(int grid[GRID_DIM][GRID_DIM], int *x, int *y)
+{
     while (check_coord_bounds(*x, *y) || grid[*x][*y] == OCC)
     {
-        cout << "Invalid coordinate pressed" << endl;
+        // TODO: Remove the details from this`
+        cout << "Invalid coordinate pressed (Either outta bounds, or already pressed)" << endl;
         ask_coords(x, y);
     }
 }
 
-// TODO: Refactor this
 void check_dir(int grid[12][12], int *x, int *y, char *dir, const int len)
 {
-    check_coord(grid, x, y);
+    constexpr int offset = 1;
+    validate_update_coords(grid, x, y);
     while (!valid_dir(*dir, len, *x, *y))
     {
-        constexpr int offset = 1;
         cout << "Invalid Direction" << endl;
-        cout << "Enter X coord: ";
-        cin >> *y;
-        cout << "Enter Y coord: ";
-        cin >> *x;
-        cout << "Enter Dir: ";
-        cin >> *dir;
-
-        check_coord(grid, x, y);
-        *x -= offset;
-        *y -= offset;
+        ask_coords_dir(x, y, dir);
+        validate_update_coords(grid, x, y);
     }
 
     for (int i = 0; i < len; i++)
@@ -187,39 +182,39 @@ void check_dir(int grid[12][12], int *x, int *y, char *dir, const int len)
         case UP:
             if (grid[*x - i][*y] == OCC)
             {
-                reask_coord(x, y, dir);
-                check_coord(grid, x, y);
+                ask_coords_dir(x, y, dir);
+                validate_update_coords(grid, x, y);
                 i = 0;
             }
             break;
         case DOWN:
             if (grid[*x + i][*y] == OCC)
             {
-                reask_coord(x, y, dir);
-                check_coord(grid, x, y);
+                ask_coords_dir(x, y, dir);
+                validate_update_coords(grid, x, y);
                 i = 0;
             }
             break;
         case RIGHT:
             if (grid[*x][*y + i] == OCC)
             {
-                reask_coord(x, y, dir);
-                check_coord(grid, x, y);
+                ask_coords_dir(x, y, dir);
+                validate_update_coords(grid, x, y);
                 i = 0;
             }
             break;
         case LEFT:
             if (grid[*x][*y - i] == OCC)
             {
-                reask_coord(x, y, dir);
-                check_coord(grid, x, y);
+                ask_coords_dir(x, y, dir);
+                validate_update_coords(grid, x, y);
                 i = 0;
             }
             break;
         default:
-            reask_coord(x, y, dir);
-            check_coord(grid, x, y);
-            i = 0;
+             ask_coords_dir(x, y, dir);
+             validate_update_coords(grid, x, y);
+             i = 0;
         }
     }
 }
@@ -229,18 +224,7 @@ void place_ships(int grid[GRID_DIM][GRID_DIM], battleship *ships[], const int nu
     for (int i = 0; i < num_ships; i++)
     {
         constexpr int offset = 1;
-        cout << "Enter X coord: ";
-        cin >> ships[i]->y;
-        ships[i]->y -= offset;
-        cout << "Enter Y coord: ";
-        cin >> ships[i]->x;
-        ships[i]->x -= offset;
-
-        check_coord(grid, &ships[i]->x, &ships[i]->y);
-
-        cout << "Enter Dir: ";
-        cin >> ships[i]->dir;
-
+        ask_coords_dir(&ships[i]->x, &ships[i]->y, &ships[i]->dir);
         check_dir(grid, &ships[i]->x, &ships[i]->y, &ships[i]->dir, ships[i]->len);
 
         // At this point coordinates should be valid...
@@ -307,7 +291,7 @@ void attack_menu(const int line, const int scan, const int mini)
 {
     cout << "Entering attack mode..." << endl;
     cout << "You currently have, " << endl;
-    cout <<  line << " Line Attacks" << endl;
+    cout << line << " Line Attacks" << endl;
     cout << scan << " Scans" << endl;
     cout << mini << " Mini-bombs" << endl;
     cout << "Remaining" << endl;
@@ -323,14 +307,37 @@ void line_attack(int grid[GRID_DIM][GRID_DIM], int atk[GRID_DIM][GRID_DIM]);
 
 void mini_attack(int grid[GRID_DIM][GRID_DIM], int atk[GRID_DIM][GRID_DIM]);
 
+typedef struct Tuple_Node
+{
+   int x;
+   int y;
+} Tuple;
+
 void scan_attack(int grid[GRID_DIM][GRID_DIM], int atk[GRID_DIM][GRID_DIM])
 {
     int x, y = -1;
+    constexpr int offset = 1;
+    Tuple coords[SCAN_WIDTH * SCAN_WIDTH];
+    int index = 0;
+
+    ask_coords(&x, &y);
+    validate_update_coords(atk, &x, &y);
+
     for (int i = 0; i < SCAN_WIDTH; i++)
     {
         for (int j = 0; j < SCAN_WIDTH; j++)
         {
+            if (check_coord_bounds(x, y) && atk[x][y] == EMP)
+            {
+                const Tuple temp { x, y };
+                coords[index] = temp;
+                index++;
+            }
         }
+    }
+    for (int i = 0; i < SCAN_WIDTH * SCAN_WIDTH; i++) // NOLINT(*-loop-convert)
+    {
+        atk[coords[i].x][coords[i].y] = SCAN;
     }
 }
 
@@ -339,28 +346,17 @@ void regular_attack(int grid[GRID_DIM][GRID_DIM], int atk[GRID_DIM][GRID_DIM])
     int x, y = -1;
     constexpr int offset = 1;
 
-    cout << "Enter X Coord: ";
-    cin >> y;
-    cout << "Enter Y Coord: ";
-    cin >> x;
+    ask_coords(&x, &y);
 
-    y -= offset;
-    x -= offset;
-    check_coord(atk, &x, &y);
+    validate_update_coords(atk, &x, &y);
 
     // 0 is a successful attack, 1 is an unsuccessful attack
     while (atk[x][y] == HIT || atk[x][y] == OCC)
     {
         cout << "Already attacked here" << endl;
-        cout << "Enter X Coord: ";
-        cin >> y;
-        cout << "Enter Y Coord: ";
-        cin >> x;
+        ask_coords(&x, &y);
 
-        y -= offset;
-        x -= offset;
-
-        check_coord(atk, &x, &y);
+        validate_update_coords(atk, &x, &y);
     }
     if (grid[x][y] == OCC)
     {
